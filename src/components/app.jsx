09e5ac4ff29a4958df6f875cec97e13067715c31
Heondo Kim <heondokim@gmail.com>
2019-11-14 21:24:17 -0800

    new build

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -14,7 +14,7 @@ import UserPage from './user/user-page';
 import Login from './user/login';
 
 export default function App(props) {
-  const [user, setUser] = useState({ id: 3 });
+  const [user, setUser] = useState({ id: undefined });
 
   return (
     <Router>
