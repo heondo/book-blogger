784610aa22fff2b97ec3a47c4712018d4bf36aa6
Heondo Kim <heondokim@gmail.com>
2020-07-16 21:51:53 -0700

    updating cancel buttons

--- src/components/user/login.jsx
+++ src/components/user/login.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
-import { Container, Grid, FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core';
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@material-ui/core";
 
 export default function Login(props) {
-  const [email, setEmail] = useState('guestuser@test.com');
-  const [password, setPassword] = useState('11111111');
+  const [email, setEmail] = useState("guestuser@test.com");
+  const [password, setPassword] = useState("11111111");
   const [loginFailed, setLoginFailed] = useState(false);
   const [validEmail, setValidEmail] = useState(true);
   const [validPassword, setValidPassword] = useState(true);
@@ -21,10 +29,10 @@ export default function Login(props) {
     return checker;
   };
 
-  const handleInputChange = e => {
+  const handleInputChange = (e) => {
     const { id, value } = e.target;
     setLoginFailed(false);
-    if (id === 'email') {
+    if (id === "email") {
       setEmail(value);
       setValidEmail(true);
       return;
@@ -33,43 +41,57 @@ export default function Login(props) {
     setValidPassword(true);
   };
 
-  const handleLogin = e => {
+  const handleLogin = (e) => {
     e.preventDefault();
     if (!areFieldsFilled()) {
       return false;
     }
-    fetch('/api/users/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password })
+    fetch("/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
     })
-      .then(res => res.json())
-      .then(res => {
+      .then((res) => res.json())
+      .then((res) => {
         console.log(res);
-        if (res.error === 'Auth failed') {
+        if (res.error === "Auth failed") {
           setLoginFailed(true);
-          throw new Error('Login Failed');
+          throw new Error("Login Failed");
         }
         // IDK IF THIS IS GOING TO RETURN THE SAME ID THING ANYMORE
         props.setUser({
           id: res.userID,
           token: res.token,
           first: res.first,
-          last: res.last
+          last: res.last,
         });
         props.history.push(`/users/${res.userID}`);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    props.history.push("/");
   };
 
   return (
     <Container>
-      <form onSubmit={handleLogin} style={{ maxWidth: '800px', margin: 'auto' }}>
-        <Grid container item justify="center" xs={12} spacing={1} style={{ marginTop: '1rem' }}>
+      <form
+        onSubmit={handleLogin}
+        style={{ maxWidth: "800px", margin: "auto" }}
+      >
+        <Grid
+          container
+          item
+          justify="center"
+          xs={12}
+          spacing={1}
+          style={{ marginTop: "1rem" }}
+        >
           <Grid item xs={8}>
-            <FormControl style={{ width: '100%' }}>
-              <InputLabel
-                htmlFor="email">Email address</InputLabel>
+            <FormControl style={{ width: "100%" }}>
+              <InputLabel htmlFor="email">Email address</InputLabel>
               <Input
                 id="email"
                 required
@@ -78,13 +100,13 @@ export default function Login(props) {
                 defaultValue="guestuser@test.com"
                 aria-describedby="emailText"
                 onChange={handleInputChange}
-                style={{ width: '100%' }} />
+                style={{ width: "100%" }}
+              />
             </FormControl>
           </Grid>
           <Grid item xs={8}>
-            <FormControl style={{ width: '100%' }}>
-              <InputLabel
-                htmlFor="password">Password</InputLabel>
+            <FormControl style={{ width: "100%" }}>
+              <InputLabel htmlFor="password">Password</InputLabel>
               <Input
                 id="password"
                 required
@@ -92,26 +114,35 @@ export default function Login(props) {
                 type="password"
                 aria-describedby="passwordText"
                 defaultValue="11111111"
-                style={{ width: '100%' }}
-                onChange={handleInputChange} />
+                style={{ width: "100%" }}
+                onChange={handleInputChange}
+              />
             </FormControl>
           </Grid>
           {loginFailed ? (
             <Grid item xs={8}>
-              <FormHelperText style={{ color: 'red' }}>
+              <FormHelperText style={{ color: "red" }}>
                 Login Failed
               </FormHelperText>
-            </Grid>)
-            : undefined
-          }
+            </Grid>
+          ) : undefined}
           <Grid container item xs={8} justify="flex-end" spacing={2}>
             <Grid item>
-              <Button color="primary" variant="contained" onClick={handleLogin}>
+              <Button
+                color="primary"
+                type="submit"
+                variant="contained"
+                onClick={handleLogin}
+              >
                 Login
               </Button>
             </Grid>
             <Grid item>
-              <Button color="secondary" type="submit" variant="contained">
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={handleCancel}
+              >
                 Cancel
               </Button>
             </Grid>
--- src/components/user/signup.jsx
+++ src/components/user/signup.jsx
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
-import { Container, Grid, FormControl, InputLabel, Input, FormHelperText, makeStyles, Button } from '@material-ui/core';
-import update from 'immutability-helper';
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+import update from "immutability-helper";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   spacingNavFirst: {
-    marginTop: '.5rem'
-  }
+    marginTop: ".5rem",
+  },
 }));
 
 export default function UserSignUp(props) {
   const classes = useStyles();
   const [inputs, setInputs] = useState({
-    firstInput: '',
-    lastInput: '',
-    emailInput: '',
-    passwordInput: '',
-    confirmPasswordInput: ''
+    firstInput: "",
+    lastInput: "",
+    emailInput: "",
+    passwordInput: "",
+    confirmPasswordInput: "",
   });
   const [validInputs, setValidInputs] = useState({
     firstInput: true,
     lastInput: true,
     emailInput: true,
     passwordInput: true,
-    confirmPasswordInput: true
+    confirmPasswordInput: true,
   });
 
   const [emailDuplicate, setEmailDuplicate] = useState(false);
 
   const verifyInputs = () => {
     let valid = true;
-    const { firstInput, lastInput, emailInput, passwordInput, confirmPasswordInput } = inputs;
+    const {
+      firstInput,
+      lastInput,
+      emailInput,
+      passwordInput,
+      confirmPasswordInput,
+    } = inputs;
     const entries = Object.entries(inputs);
     let newValidInputs = Object.assign({}, validInputs);
     for (let input of entries) {
       if (!input[1]) {
         valid = false;
         newValidInputs = update(newValidInputs, {
-          [input[0]]: { $set: false }
+          [input[0]]: { $set: false },
         });
       }
     }
@@ -44,66 +59,66 @@ export default function UserSignUp(props) {
       valid = false;
       newValidInputs = update(newValidInputs, {
         confirmPasswordInput: { $set: false },
-        passwordInput: { $set: false }
+        passwordInput: { $set: false },
       });
       setValidInputs(newValidInputs);
     }
     if (passwordInput.length < 8) {
       valid = false;
       newValidInputs = update(newValidInputs, {
-        passwordInput: { $set: false }
+        passwordInput: { $set: false },
       });
     }
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)) {
       valid = false;
       newValidInputs = update(newValidInputs, {
-        emailInput: { $set: false }
+        emailInput: { $set: false },
       });
     }
     setValidInputs(newValidInputs);
     return valid;
   };
 
-  const submitNewUser = e => {
+  const submitNewUser = (e) => {
     e.preventDefault();
     if (!verifyInputs()) {
       return;
     }
-    fetch('/api/users/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(
-        inputs
-      )
+    fetch("/api/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inputs),
     })
-      .then(res => res.json())
-      .then(res => {
-        if (res.error === 'Email already taken') {
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error === "Email already taken") {
           setEmailDuplicate(true);
-          setValidInputs(prev => update(prev, {
-            emailInput: { $set: false }
-          }));
-          throw new Error('email taken');
+          setValidInputs((prev) =>
+            update(prev, {
+              emailInput: { $set: false },
+            })
+          );
+          throw new Error("email taken");
         }
         props.setUser({
           id: res.id,
           first: res.first,
-          last: res.last
+          last: res.last,
         });
         props.history.push(`/users/${res.id}`);
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
     // return true;
     // make a request to users to sign up a user
   };
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { id, value } = event.target;
-    if (id === 'emailInput') {
+    if (id === "emailInput") {
       setEmailDuplicate(false);
     }
     const newInput = update(inputs, {
-      [id]: { $set: value }
+      [id]: { $set: value },
     });
     setInputs(newInput);
     setValidInputs({
@@ -111,100 +126,133 @@ export default function UserSignUp(props) {
       lastInput: true,
       emailInput: true,
       passwordInput: true,
-      confirmPasswordInput: true
+      confirmPasswordInput: true,
     });
   };
 
+  const handleCancel = (e) => {
+    e.preventDefault();
+    props.history.push("/");
+  };
+
   return (
-    <Container style={{ display: 'flex', justifyContent: 'center' }}>
+    <Container style={{ display: "flex", justifyContent: "center" }}>
       <form onSubmit={submitNewUser}>
-        <Grid container item xs={8} spacing={1} className={classes.spacingNavFirst} style={{ margin: 'auto' }}>
+        <Grid
+          container
+          item
+          xs={8}
+          spacing={1}
+          className={classes.spacingNavFirst}
+          style={{ margin: "auto" }}
+        >
           <Grid item xs={6}>
-            <FormControl
-              style={{ width: '100%' }}>
-              <InputLabel
-                htmlFor="firstInput">First</InputLabel>
+            <FormControl style={{ width: "100%" }}>
+              <InputLabel htmlFor="firstInput">First</InputLabel>
               <Input
                 id="firstInput"
                 error={!validInputs.firstInput}
                 aria-describedby="firstNameInput"
                 width={1}
-                onChange={handleInputChange} />
+                onChange={handleInputChange}
+              />
               {!validInputs.firstInput ? (
-                <FormHelperText id="first-helper">Please enter a first name</FormHelperText>
-              ) : (
-                undefined
-              )}
+                <FormHelperText id="first-helper">
+                  Please enter a first name
+                </FormHelperText>
+              ) : undefined}
             </FormControl>
           </Grid>
           <Grid item xs={6}>
-            <FormControl style={{ width: '100%' }}>
+            <FormControl style={{ width: "100%" }}>
               <InputLabel htmlFor="lastInput">Last</InputLabel>
               <Input
                 id="lastInput"
                 error={!validInputs.lastInput}
                 aria-describedby="lastNameInput"
-                onChange={handleInputChange} />
+                onChange={handleInputChange}
+              />
               {!validInputs.lastInput ? (
-                <FormHelperText id="last-helper">Please enter a last name</FormHelperText>
-              ) : (
-                undefined
-              )}
+                <FormHelperText id="last-helper">
+                  Please enter a last name
+                </FormHelperText>
+              ) : undefined}
             </FormControl>
           </Grid>
           <Grid item xs={12}>
-            <FormControl
-              style={{ width: '100%' }}>
-              <InputLabel
-                htmlFor="emailInput">Email address</InputLabel>
+            <FormControl style={{ width: "100%" }}>
+              <InputLabel htmlFor="emailInput">Email address</InputLabel>
               <Input
                 id="emailInput"
                 error={!validInputs.emailInput}
                 type="email"
                 aria-describedby="emailInputText"
-                onChange={handleInputChange} />
+                onChange={handleInputChange}
+              />
               {!validInputs.emailInput ? (
-                <FormHelperText id="email-helper">{emailDuplicate ? 'Email already taken' : 'Invalid email'}</FormHelperText>
+                <FormHelperText id="email-helper">
+                  {emailDuplicate ? "Email already taken" : "Invalid email"}
+                </FormHelperText>
               ) : (
-                <FormHelperText id="email-helper">We'll never share your email.</FormHelperText>
+                <FormHelperText id="email-helper">
+                  We'll never share your email.
+                </FormHelperText>
               )}
             </FormControl>
           </Grid>
           <Grid item xs={12}>
-            <FormControl style={{ width: '100%' }}>
+            <FormControl style={{ width: "100%" }}>
               <InputLabel htmlFor="passwordInput">Password</InputLabel>
-              <Input id="passwordInput"
+              <Input
+                id="passwordInput"
                 error={!validInputs.passwordInput}
-                aria-describedby="passwordInputText" type="password" onChange={handleInputChange} />
+                aria-describedby="passwordInputText"
+                type="password"
+                onChange={handleInputChange}
+              />
               {!validInputs.passwordInput ? (
-                <FormHelperText id="password-helper">Not long enough or no match</FormHelperText>
-              ) : (
-                undefined
-              )}
+                <FormHelperText id="password-helper">
+                  Not long enough or no match
+                </FormHelperText>
+              ) : undefined}
             </FormControl>
           </Grid>
           <Grid item xs={12}>
-            <FormControl style={{ width: '100%' }}>
-              <InputLabel htmlFor="confirmPasswordInput">Confirm Password</InputLabel>
-              <Input id="confirmPasswordInput"
+            <FormControl style={{ width: "100%" }}>
+              <InputLabel htmlFor="confirmPasswordInput">
+                Confirm Password
+              </InputLabel>
+              <Input
+                id="confirmPasswordInput"
                 type="password"
                 error={!validInputs.confirmPasswordInput}
-                aria-describedby="confirmPasswordInputText" onChange={handleInputChange} />
+                aria-describedby="confirmPasswordInputText"
+                onChange={handleInputChange}
+              />
               {!validInputs.confirmPasswordInput ? (
-                <FormHelperText id="confirm-password-helper">Password does not match</FormHelperText>
-              ) : (
-                undefined
-              )}
+                <FormHelperText id="confirm-password-helper">
+                  Password does not match
+                </FormHelperText>
+              ) : undefined}
             </FormControl>
           </Grid>
           <Grid container item xs={12} justify="flex-end" spacing={2}>
             <Grid item>
-              <Button onClick={submitNewUser} color="primary" variant="contained">
+              <Button
+                type="submit"
+                onClick={submitNewUser}
+                color="primary"
+                variant="contained"
+              >
                 Register
               </Button>
             </Grid>
             <Grid item>
-              <Button color="secondary" variant="contained" type="submit">
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={handleCancel}
+              >
                 Cancel
               </Button>
             </Grid>
